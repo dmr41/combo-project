75e4a981cdad214179baede16557d7da487d38b5
David Rivers <dmr41.rivers@gmail.com>
2016-02-08 11:57:49 -0700

    added normal view back to project

--- routes/normal.js
+++ routes/normal.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var wkhtmltopdf = require('node-wkhtmltopdf');
+
 var fs = require('fs');
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	// var rstream = fs.createReadStream('zayo.html');
-  // rstream.pipe(res);
+	var rstream = fs.createReadStream('zayo.html');
+  rstream.pipe(res);
 });
 
 module.exports = router;
